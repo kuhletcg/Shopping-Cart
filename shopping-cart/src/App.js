aed7497af83ef74c2b52167ab4bdd5998b580146
TCG <ssibonokuhle@gmail.com>
2021-02-09 09:12:27 +0200

    imported four components

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Basket from './Components/Basket';
+import data from './data';
+
 
 function App() {
+
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find(x => x.id ===  product.id);
+    if (exist) {
+      setCartItems (cartItems.map(x => x.id === product.id ? {...exist, qty: exist.qty +1 } : x
+        )
+        );
+    }else {
+      setCartItems([...cartItems, ...product, qty: 1]);
+    }
+  };
   return (
     <div className="App">
-    <h1>Kuhle</h1>
+ <Header></Header>
+ <div className= "row">
+   <Main onAdd={onAdd} products = { products}></Main>
+   <Basket onAdd={onAdd} cartItems= {cartItems}></Basket>
+ </div>
     </div>
   );
 }
