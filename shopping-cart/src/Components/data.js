f72f8fa4282a7c4fbb8ac640a1bba4f08a226ea0
TCG <ssibonokuhle@gmail.com>
2021-02-10 10:57:48 +0200

    Added products

--- shopping-cart/src/Components/data.js
+++ shopping-cart/src/Components/data.js
@@ -1,15 +1,89 @@
+// const data = {
+//   products: [
+//     {
+//       id: '1',
+//       name: 'MacBook',
+//       price: 1400,
+//       image: 'https://picsum.photos/id/180/2400/1600',
+//     },
+//     {
+//       id: '2',
+//       name: 'dress',
+//       price: 250,
+//       image: 'https://cdn.shopify.com/s/files/1/2497/3826/products/ea3f3be4-989b-457e-b54b-c775e8499ed5_1024x1024@2x.JPG?v=1536327061',
+//     },
+//     {
+//       id: '3',
+//       name: 'Television',
+//       price: 2500,
+//       image: 'https://www.takealot.com/omega-32-hd-ready-led-tv/PLID52124242',
+//     },
+
+//      {
+//       id: '4',
+//       name: 'Television',
+//       price: 2500,
+//       image: 'https://www.takealot.com/omega-32-hd-ready-led-tv/PLID52124242',
+//     },
+
+//      {
+//       id: '3',
+//       name: 'Television',
+//       price: 2500,
+//       image: 'https://www.takealot.com/omega-32-hd-ready-led-tv/PLID52124242',
+//     },
+//      {
+//       id: '5',
+//       name: 'Television',
+//       price: 2500,
+//       image: 'https://www.takealot.com/omega-32-hd-ready-led-tv/PLID52124242',
+//     },
+//      {
+//       id: '6',
+//       name: 'Television',
+//       price: 2500,
+//       image: 'https://www.takealot.com/omega-32-hd-ready-led-tv/PLID52124242',
+//     },
+//      {
+//       id: '7',
+//       name: 'Bed',
+//       price: 1500,
+//       image: 'https://www.takealot.com/quality-reliable-foam-base-and-mattress-standard-length-188cm-single/PLID58970794',
+//     },
+//      {
+//       id: '8',
+//       name: 'Ladies short',
+//       price: 300,
+//       image: 'https://www.takealot.com/roxy-wave-7-inch-women-s-board-short-anthracite/PLID70437816',
+//     },
+//      {
+//       id: '10',
+//       name: 'iPhone  11',
+//       price: 13999,
+//       image: 'https://www.takealot.com/omega-32-hd-ready-led-tv/PLID52124242',
+//     },
+//       {
+//       id: '10',
+//       name: 'Duvet',
+//       price: 700,
+//       image: 'https://www.takealot.com/linen-boutique-duvet-cover-200-tc-4-piece-set-strips-lightlavendergrey/PLID71234582',
+//     },
+//   ],
+// };
+// export default data;
+
 const data = {
-    Products: [
+    products: [
         {
             id: '1',
     name: 'dress',
-Price: '1344',
+Price: 1344,
 image: 'https://cdn.shopify.com/s/files/1/2497/3826/products/ea3f3be4-989b-457e-b54b-c775e8499ed5_1024x1024@2x.JPG?v=1536327061',
 },
 
        {   id: '2',
     name: 'earphones',
-Price: '1344',
+Price: 1344,
 image: 'https://media.takealot.com/covers_images/19083c43e8e74746b720c03a9cebd9f1/s-pdpxl.file',
 },
 
