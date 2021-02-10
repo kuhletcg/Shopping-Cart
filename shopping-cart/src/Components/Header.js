f1057be1e8bf7d0bbabba26edd0256fb91d3c907
TCG <ssibonokuhle@gmail.com>
2021-02-10 10:54:34 +0200

    Maped product

--- shopping-cart/src/Components/Header.js
+++ shopping-cart/src/Components/Header.js
@@ -1,31 +1,56 @@
 import React from 'react';
 
-
 export default function Header(props) {
-    const {countCartItems} = props;
-    return (
-        <header className="row block center">
-            <div>
-                <a href="#/">
-                    <h1> Small Shopping Cart</h1>
-                </a>
-            </div>
-<div>
-            <a href="#/cart">
-                
-                
-                CArt { ''}
-                {countCartItems? (
-                    <button className="badge">{countCartItems}</button>
-                ) : (
-                    ''
-                )
-                }
-                
-                
-                </a> {' '}
-            <a href="#/signin">SignIn</a>
-         </div>
-        </header>
-    );
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/">
+          <h1>Small Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        <a href="#/signin"> SignIn</a>
+      </div>
+    </header>
+  );
 }
+
+// import React from 'react';
+
+
+// export default function Header(props) {
+//     const {countCartItems} = props;
+//     return (
+//         <header className="row block center">
+//             <div>
+//                 <a href="#/">
+//                     <h1> Small Shopping Cart</h1>
+//                 </a>
+//             </div>
+// <div>
+//             <a href="#/cart">
+                
+                
+//                 CArt { ''}
+//                 {countCartItems? (
+//                     <button className="badge">{countCartItems}</button>
+//                 ) : (
+//                     ''
+//                 )
+//                 }
+                
+                
+//                 </a> {' '}
+//             <a href="#/signin">SignIn</a>
+//          </div>
+//         </header>
+//     );
+// }
--- shopping-cart/src/Components/Main.js
+++ shopping-cart/src/Components/Main.js
@@ -1,18 +1,18 @@
-import React from 'react';
 
+import React from 'react';
+import Product from './Product';
 
 export default function Main(props) {
-    return (
-        <header>
-            <div>
-                <a href="#/">
-                    <h1>Shopping Cart</h1>
-                </a>
-            </div>
-<div>
-            <a href="#/cart">CArt</a>
-            <a href="#/signin">SignIn</a>
-         </div>
-        </header>
-    );
+  const { products, onAdd } = props;
+  return (
+    <main className="block col-2">
+      <h2>Products</h2>
+      <div className="row">
+        {products.map((product) => (
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+        ))}
+      </div>
+    </main>
+  );
 }
+
