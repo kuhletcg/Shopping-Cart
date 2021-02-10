196f57eaf3f1a07c67afb0b87a12666788190ebf
TCG <ssibonokuhle@gmail.com>
2021-02-10 10:44:51 +0200

    Import app css

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,44 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Basket from './Components/Basket';
-import data from './data';
-
-
+import data from './Components/data';
+import { useState } from 'react';
 function App() {
-
   const { products } = data;
+  console.log (products)  
   const [cartItems, setCartItems] = useState([]);
   const onAdd = (product) => {
-    const exist = cartItems.find(x => x.id ===  product.id);
+    const exist = cartItems.find((x) => x.id === product.id);
     if (exist) {
-      setCartItems (cartItems.map(x => x.id === product.id ? {...exist, qty: exist.qty +1 } : x
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
         )
-        );
-    }else {
-      setCartItems([...cartItems, ...product, qty+ 1]);
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
     }
   };
-
   const onRemove = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist.qty === 1) {
       setCartItems(cartItems.filter((x) => x.id !== product.id));
     } else {
       setCartItems(
-        cartItems.map ((x) =>
-        x.id === product.id ? {...exist,qty: exist.qty -1} : x)
-      )
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
     }
-  }
+  };
   return (
     <div className="App">
- <Header></Header>
- <div className= "row">
-   <Main onAdd={onAdd} products = { products}></Main>
-   <Basket onAdd={onAdd} onRemove={onRemove} cartItems= {cartItems}></Basket>
- </div>
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Basket>
+      </div>
     </div>
   );
 }
